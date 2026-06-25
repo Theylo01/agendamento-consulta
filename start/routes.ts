60edb96621ab534ab47b9e937da9b486d39ba8d2
theylo <italo.theylor@gmail.com>
2026-06-24 21:45:48 -0300

    rotas do sistema

--- start/routes.ts
+++ start/routes.ts
@@ -1,30 +1,8 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
-import { middleware } from '#start/kernel'
-import { controllers } from '#generated/controllers'
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home').as('home')
+const AppointmentsController = () => import('#controllers/appointments_controller')
 
-router
-  .group(() => {
-    router.get('signup', [controllers.NewAccount, 'create'])
-    router.post('signup', [controllers.NewAccount, 'store'])
+router.get('/', [AppointmentsController, 'index']).as('home')
+router.post('/appointments', [AppointmentsController, 'store'])
 
-    router.get('login', [controllers.Session, 'create'])
-    router.post('login', [controllers.Session, 'store'])
-  })
-  .use(middleware.guest())
-
-router
-  .group(() => {
-    router.post('logout', [controllers.Session, 'destroy'])
-  })
-  .use(middleware.auth())
+router.post('/appointments/:id/delete', [AppointmentsController, 'destroy'])
